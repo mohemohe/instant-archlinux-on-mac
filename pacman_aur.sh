6ae833554bef23a0eb8a74c21bdcad9098666c66
もっへもへ <mohemohe@users.noreply.github.com>
2017-07-08 17:29:52 +0900

    Update pacman_aur.sh

--- pacman_aur.sh
+++ pacman_aur.sh
@@ -1,9 +1,9 @@
 # Install from Antergos repo
 
-pacman -S wget
+pacman -S wget --noconfirm
 
 wget http://repo.antergos.com/antergos/x86_64/antergos-keyring-20170524-1-any.pkg.tar.xz -O /tmp/antergos-keyring-20170524-1-any.pkg.tar.xz
-pacman -U /tmp/antergos-keyring-20170524-1-any.pkg.tar.xz
+pacman -U /tmp/antergos-keyring-20170524-1-any.pkg.tar.xz --noconfirm
 pacman-key --init archlinux antergos && sudo pacman-key --populate archlinux antergos
 
 cat > /etc/pacman.conf <<EOS
@@ -13,4 +13,5 @@ Usage = All
 Server = http://repo.antergos.com/$repo/$arch
 EOS
 
-pacman -Syy yajl yaourt
+pacman -Syy z --noconfirm
+pacman -S yajl yaourt --noconfirm
