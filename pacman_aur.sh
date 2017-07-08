df9a80d292915b4c563b2179cce1f6d8829adeed
もっへもへ <mohemohe@users.noreply.github.com>
2017-07-08 17:42:14 +0900

    Update pacman_aur.sh

--- pacman_aur.sh
+++ pacman_aur.sh
@@ -3,14 +3,15 @@
 pacman -S wget --noconfirm
 
 wget http://repo.antergos.com/antergos/x86_64/antergos-keyring-20170524-1-any.pkg.tar.xz -O /tmp/antergos-keyring-20170524-1-any.pkg.tar.xz
-pacman -U /tmp/antergos-keyring-20170524-1-any.pkg.tar.xz --noconfirm
+wget http://repo.antergos.com/antergos/x86_64/antergos-mirrorlist-20170527-2-any.pkg.tar.xz -O /tmp/antergos-mirrorlist-20170527-2-any.pkg.tar.xz
+
+pacman -U /tmp/antergos-keyring-20170524-1-any.pkg.tar.xz /tmp/antergos-keyring-20170524-1-any.pkg.tar.xz --noconfirm
 pacman-key --init archlinux antergos && sudo pacman-key --populate archlinux antergos
 
 cat > /etc/pacman.conf <<EOS
 [antergos]
 SigLevel = PackageRequired
-Usage = All
-Server = http://repo.antergos.com/$repo/$arch
+Include = /etc/pacman.d/antergos-mirrorlist
 EOS
 
 pacman -Syy z --noconfirm
